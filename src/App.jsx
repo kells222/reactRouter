d5c2abaf821d4621f4fdc85020ecc8b8eb49e391
Kelly Buckley <princesskells@Kellys-MacBook-Air.local>
2023-08-13 16:49:41 -0700

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import './App.css'
+import Blue from './components/Blue'
+import Red from './components/Red'
+import Home from './components/Home'
 
 function App() {
 
@@ -9,11 +12,16 @@ function App() {
     <>
     <div id="container">
       <h1></h1>
-    <div id="navbar">{/* navigation here */}</div>
+    <div id="navbar">
+      <Link to={'/'}>Home</Link>
+      <Link to={'/red'}>Red</Link>
+      <Link to={'/blue'}>Blue</Link>
+    </div>
     <div id="main-section">
       <Routes>
-        <Route path='/blue' element={<h1>Blue</h1>}/>
-        <Route path='/red' element={<h1>Red</h1>}/>
+        <Route path='/red' element={<Red/>}/>
+        <Route path='/blue' element={<Blue/>}/>
+        <Route path='/' element={<Home/>}/>
       </Routes>
     </div>
     </div>
